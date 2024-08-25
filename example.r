27fb8fb87ec79678eb417a0e052e0a67febbfe49
Jiefei Wang <szwjf08@gmail.com>
2024-08-25 11:25:21 -0500

    update

--- example.r
+++ example.r
@@ -1,36 +1,30 @@
 source("setup.r",local =TRUE)
 # devtools::install_github('jtleek/tidypvals')
-
-
 library(tidypvals)
 library(tidyverse)
-jager2014|>
-group_by(year)|>
-summarize(n=n())
+
 
 papers <- jager2014|>
 filter(year==2010)
 
-length(unique(papers$pmid))
 
-
-papers<- papers|>
+## get p-values
+pvals<- papers|>
 pull(pvalue)
-
-## factor to its actual value
 pvals <- as.numeric(levels(pvals)[pvals])
 length(pvals)
 
-sum(pvals<=0.05)
-
 
 alpha <- 0.05
+q <- 0.05
 n <- length(pvals)
 lambda <- 0.5
 
+## pi0 estimators, with lambda = 0.5
 pi_fun <- pi_generator(lambda, pvals)
 res_GCB <- generalExceedance::pi_BJ_fast(pi_fun, pvals, 0.05, c(0,1), c(0,1))$upper
 
+## other methods for comparison
 methods <- c("abh", "st.spline", "st.boot", "langaas", "jiang")
 res_others <- sapply(methods, function(method) cp4p::estim.pi0(pvals, pi0.method = method)[[1]])
 
